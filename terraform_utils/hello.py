d2ca5709c5acfafc928b33e1574b1a5f2bfcef52
Rajan Khullar <rkhullar03@gmail.com>
2019-11-22 21:16:51 -0500

    improve example data test

--- terraform_utils/hello.py
+++ terraform_utils/hello.py
@@ -1,15 +1,8 @@
 from pathlib import Path
 
-here = Path(__file__).parent
 
-data = here / 'data'
-target = data / 'hello.txt'
-
-
-def test():
+def read_data() -> str:
+    here: Path = Path(__file__).parent
+    target = here / 'data' / 'hello.txt'
     with target.open('r') as f:
         return f.read().strip()
-
-
-if __name__ == '__main__':
-    print(test())
--- terraform_utils/tests/test_hello.py
+++ terraform_utils/tests/test_hello.py
@@ -1,7 +1,12 @@
-import unittest
+from nose.tools import assert_equal
+from unittest import TestCase
+from ..hello import read_data
 
 
-class TestHello(unittest.TestCase):
+class HelloTest(TestCase):
 
     def test_canary(self):
         self.assertEqual(1, 1)
+
+    def test_read_data(self):
+        assert_equal(read_data(), 'hello world')

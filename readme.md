d61a6a71588796dbfd58c4293bd253e7404acb1f
Rajan Khullar <rkhullar03@gmail.com>
2019-11-23 13:51:26 -0500

    testing utils

--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
 ## Terraform Utils
+
+### Notes
+``` sh
+docker run -d --name sonarqube -p 9000:9000 sonarqube
+nosetests --with-xunit --with-coverage --cover-xml
+sonar-scanner -D project.settings=cicd/sonar-project.properties
+```
--- terraform_utils/tests/unit/test_utils.py
+++ terraform_utils/tests/unit/test_utils.py
@@ -31,6 +31,12 @@ class UtilsTest(TestCase):
         assert_is_not_none(match_object)
         assert_equal(dict(key=key, val=val), match_object.groupdict())
 
+    def test_variable_list(self):
+        data = {'x': '1', 'y': '2'}
+        expected = {Variable('x', '1'), Variable('y', '2')}
+        actual = set(Variable.from_dict(data))
+        assert_equal(expected, actual)
+
     def test_find_target(self):
         with tempfile.TemporaryDirectory() as temp_dir:
             live_dir = Path(temp_dir) / 'live'
@@ -41,3 +47,11 @@ class UtilsTest(TestCase):
     def test_load_config(self):
         target = Path(__file__).parents[2] / 'data' / 'common.tfvars'
         assert_equal(build_default_common_values(), load_config(target))
+
+    def test_write_common_values(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            target = Path(temp_dir) / 'common.tfvars'
+            values = build_default_common_values()
+            write_common_values(target, values)
+            config = load_config(target)
+            assert_equal(config, values)
